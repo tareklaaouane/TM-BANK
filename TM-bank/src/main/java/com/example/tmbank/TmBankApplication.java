18bcbc82af6c605a22c87faceecd613aceb15b7c
tareklaaouane <tlaaouane@gmail.com>
2024-01-15 22:12:32 +0100

    first commit

---
+++ TM-bank/src/main/java/com/example/tmbank/TmBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmBankApplication.class, args);
+	}
+
+}
---
+++ TM-bank/src/test/java/com/example/tmbank/TmBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
